7b8c3b91a2113d310a9b44216352f839f6afdf90
Ivan <ivanguerreschi86@gmail.com>
2019-12-12 23:43:48 +0100

    menu string

--- main.rb
+++ main.rb
@@ -1,21 +1,21 @@
-# This file is part of ComicsRuby.
-#
-# ComicsPerl is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# ComicsPerl is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
+#This program is free software: you can redistribute it and/or modify
+#it under the terms of the GNU General Public License as published by
+#the Free Software Foundation, either version 3 of the License, or
+#(at your option) any later version.
 
-# You should have received a copy of the GNU General Public License
-# along with Nome-Programma.  If not, see <http://www.gnu.org/licenses/>.
+#This program is distributed in the hope that it will be useful,
+#but WITHOUT ANY WARRANTY; without even the implied warranty of
+#MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#GNU General Public License for more details.
 #
-# Copyright 2019 Ivan Guerreschi
+#You should have received a copy of the GNU General Public License
+#along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#
+#Email ivanguerreschi86@gmail.com
+#
+#Copyright (C) 2019  Ivan Guerreschi
 
-menu = <<EOD
+menu = <<EOF
 [p] List of comics
 [i] Enter comic book
 [e] Delete comic
@@ -23,6 +23,6 @@ menu = <<EOD
 [r] Replace
 [o] Sort
 [q] Quit
-EOD
+EOF
 
 puts menu
